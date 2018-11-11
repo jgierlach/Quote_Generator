bb2f4e0f6eee647917c4b50d84827892ff92ba52
jgierlach <jegierlach@gmail.com>
2018-11-10 20:32:53 -0500

    materialize css installed in project

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1 className="teal lighten-2 class">BOILERMAKER</h1>
     <nav>
       {isLoggedIn ? (
         <div>
